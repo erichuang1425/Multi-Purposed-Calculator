1ff5e0d9882db655a8b2234ccc662797ae77e44a
erichuang1425 <61899328+erichuang1425@users.noreply.github.com>
2020-05-27 13:46:19 +0700

    Initial Commit

---
+++ Xcode Terminal Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode Terminal Project
+//
+//  Created by Eric Huang on 5/27/20.
+//  Copyright © 2020 Eric Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
